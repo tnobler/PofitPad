a4ec39cc75e5b7d1edd030b46dd050d508194db0
Tanner Noble <tannernoble@gmail.com>
2020-10-29 17:01:48 -0500

    Add Login and Signup components and update Signup form. Refactor Register to Signup.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Signup from './components/auth/Signup';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/signup' component={Signup} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
