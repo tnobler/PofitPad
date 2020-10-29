56ce9b0396f5c0b63558eb6074ad46c0ff5ec9dd
Tanner Noble <tannernoble@gmail.com>
2020-10-29 17:22:52 -0500

    Create Redux Store and wire it up to App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,31 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/signup' component={Signup} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/signup' component={Signup} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
