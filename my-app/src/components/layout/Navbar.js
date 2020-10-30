8baa62a86a19e3526b596562a90b4ab2d49c6ff7
Tanner Noble <tannernoble@gmail.com>
2020-10-30 11:12:12 -0500

    Logout & Navbar Links

--- my-app/src/components/layout/Navbar.js
+++ my-app/src/components/layout/Navbar.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to='/properties'>Properties</Link>
+      </li>
+      <li>
+        <Link onClick={logout} to='/login'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </Link>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to='#!'>Users</Link>
+      </li>
+      <li>
+        <Link to='/signup'>Sign Up</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,19 +39,21 @@ const Navbar = () => {
           <i className='fas fa-house-damage'></i> ProfitPad
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to='/properties'>Properties</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Sign Up</Link>
-        </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-      </ul>
+
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
