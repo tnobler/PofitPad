898f6c460ca1d9d7d88554dd7584a84dc8052f28
Tanner Noble <tannernoble@gmail.com>
2020-10-30 14:53:03 -0500

    Profile Reducer & Get Current Profile.

--- my-app/src/components/dashboard/Dashboard.js
+++ my-app/src/components/dashboard/Dashboard.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-export const Dashboard = () => {
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
   return <div>Dashboard</div>;
 };
 
 Dashboard.propTypes = {
-  prop: PropTypes
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
 
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
