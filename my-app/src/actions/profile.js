e67b061b7ef02a825710bc5a93c31c6e0d173276
Tanner Noble <tannernoble@gmail.com>
2020-11-02 09:37:04 -0600

    Get All Profiles and Get Profile By User ID actions.

--- my-app/src/actions/profile.js
+++ my-app/src/actions/profile.js
@@ -4,6 +4,7 @@ import { setAlert } from './alert';
 import {
   CLEAR_PROFILE,
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   ACCOUNT_DELETED
 } from './types';
@@ -12,6 +13,42 @@ import {
 export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get('/api/v1/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get('/api/v1/profile');
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get profile by ID
+export const getProfileById = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/v1/profile/${userId}`);
 
     dispatch({
       type: GET_PROFILE,
