8d95a42ce82c178acb23bedb655df3b8318df6df
Tanner Noble <tannernoble@gmail.com>
2020-11-02 13:50:12 -0600

    ProfileTop & ProfileAbout Components.

---
+++ my-app/src/components/profile/ProfileAbout.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = ({
+  profile: {
+    bio,
+    user: { name }
+  }
+}) => {
+  return (
+    <div class='profile-about bg-light p-2'>
+      {bio && (
+        <Fragment>
+          <h2 class='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
+          <p>{bio}</p>
+          <div class='line'></div>
+        </Fragment>
+      )}
+    </div>
+  );
+};
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+export default ProfileAbout;
