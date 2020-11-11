1ac8e2a80b939cbc0112e1df1046aee6ff3175fd
Tanner Noble <tannernoble@gmail.com>
2020-11-11 15:31:09 -0600

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,14 +10,22 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/v1/users', require('./routes/api/v1/users'));
 app.use('/api/v1/auth', require('./routes/api/v1/auth'));
 app.use('/api/v1/profile', require('./routes/api/v1/profile'));
 app.use('/api/v1/properties', require('./routes/api/v1/properties'));
 // app.use('/api/v1/budgets', require('./routes/api/v1/budgets'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('my-app/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
